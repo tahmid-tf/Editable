be670bc1b197729c92bcd305c9c68a8965799dc1
Tahmid Ferdous <Tahmid>
2024-05-27 16:44:21 +0600

    login, registration and user route api integrated with proper validation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,74 +9,153 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function user()
     {
-        return Auth::user();
+        $user = Auth::user();
+
+        if ($user) {
+            return response()->json([
+                'status' => 'success',
+                'data' => $user,
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found',
+            ], 404);
+        }
+
     }
 
     public function register(Request $request)
     {
-        $inputs['email'] = $request->input('email');
-        $inputs['phone'] = $request->input('phone');
-        $inputs['name'] = $request->input('name');
-        $inputs['password'] = $request->input('password');
 
-        // Check if the email already exists in the database
 
-        if (User::where('email', $inputs['email'])->exists()) {
+//        ------------------------------------------------- validation block -------------------------------------------------
+
+
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required',
+                'phone' => 'required',
+                'name' => 'required',
+                'password' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
+            }
+
+            $inputs = $validator->validated();
+
+//        ------------------------------------------------- validation block -------------------------------------------------
+
+//        -------------------------------- Rest of the codes --------------------------------
+
+
+            // Check if the email already exists in the database
+
+            if (User::where('email', $inputs['email'])->exists()) {
+                return response([
+                    'message' => 'Email already exists'
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            // Check if the phone no already exists in the database
+
+            if (User::where('phone', $inputs['phone'])->exists()) {
+                return response([
+                    'message' => 'Phone number already exists'
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            $user = User::create([
+                'name' => $inputs['name'],
+                'email' => $inputs['email'],
+                'phone' => $inputs['phone'],
+                'password' => Hash::make($inputs['password'])
+            ]);
+
+            if (Auth::attempt(['phone' => $request->input('phone'), 'password' => $request->input('password')])) {
+                $user = Auth::user();
+                $token = $user->createToken('token')->plainTextToken;
+                $cookie = cookie('jwt', $token, 60 * 24 * 30); // 30 days
+
+                return response([
+                    'token' => $token,
+                    'user' => $user,
+                ])->withCookie($cookie);
+            }
+
             return response([
-                'message' => 'Email already exists'
-            ], Response::HTTP_BAD_REQUEST);
+                'message' => 'Invalid Credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+
+//        -------------------------------- Rest of the codes --------------------------------
+
+
+//        ------------------------------------------------- validation block -------------------------------------------------
+
+
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->validator->errors()], 422);
         }
 
-        // Check if the phone no already exists in the database
 
-        if (User::where('phone', $inputs['phone'])->exists()) {
-            return response([
-                'message' => 'Phone number already exists'
-            ], Response::HTTP_BAD_REQUEST);
-        }
+//        ------------------------------------------------- validation block -------------------------------------------------
 
-        $user = User::create([
-            'name' => $inputs['name'],
-            'email' => $inputs['email'],
-            'phone' => $inputs['phone'],
-            'password' => Hash::make($inputs['password'])
-        ]);
-
-        if (Auth::attempt(['phone' => $request->input('phone'), 'password' => $request->input('password')])) {
-            $user = Auth::user();
-            $token = $user->createToken('token')->plainTextToken;
-            $cookie = cookie('jwt', $token, 60 * 24 * 30); // 30 days
-
-            return response([
-                'token' => $token,
-                'user' => $user,
-            ])->withCookie($cookie);
-        }
-
-        return response([
-            'message' => 'Invalid Credentials'
-        ], Response::HTTP_UNAUTHORIZED);
 
     }
 
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password'))) {
+
+        //        ------------------------------------------------- validation block -------------------------------------------------
+
+
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required',
+                'password' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
+            }
+
+            $inputs = $validator->validated();
+
+            //        ------------------------------------------------- validation block -------------------------------------------------
+
+
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response([
+                    'message' => 'Inavalid Credentials'
+                ], Response::HTTP_UNAUTHORIZED);
+            };
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+            $cookie = cookie('jwt', $token, 60 * 24 * 30); //30days
             return response([
-                'message' => 'Inavalid Credentials'
-            ], Response::HTTP_UNAUTHORIZED);
-        };
-        $user = Auth::user();
-        $token = $user->createToken('token')->plainTextToken;
-        $cookie = cookie('jwt',$token,60*24*30); //30days
-        return response([
-            'message' => $token
-        ])->withCookie($cookie);
+                'token' => $token,
+                'user' => $user,
+            ])->withCookie($cookie);
+
+            //        ------------------------------------------------- validation block -------------------------------------------------
+
+
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->validator->errors()], 422);
+        }
+
+
+        //        ------------------------------------------------- validation block -------------------------------------------------
+
 
     }
 
