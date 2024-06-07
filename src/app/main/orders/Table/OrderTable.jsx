6475dc4c01a329bad6609ae7c265390ba55414d0
rased <rased100@gmail.com>
2024-06-07 18:14:12 +0600

    update table show all columns -running

--- src/app/main/orders/Table/OrderTable.jsx
+++ src/app/main/orders/Table/OrderTable.jsx
@@ -29,45 +29,28 @@ const columns = [
   "Order Status",
   "", // this field for icons
 ];
-// const columnsAll = [
-//   "Order Date",
-//   "Order ID",
-//   "Order Name",
-//   "Order Type",
-//   "Users Name",
-//   "User Email",
-//   "User Phone",
-//   "Delivery Date",
-//   "Price",
-//   "Payment Status",
-//   "Editor",
-//   "Files",
-//   "Order Status",
-//   "Download Link",
-//   "", // this field for icons
-// ];
 
-const rows = [
-  // {
-  //   date: "Order Date",
-  //   id: "Order ID",
-  //   orderName: "Order Name",
-  //   orderType: "Order Type",
-  //   usersName: "Users Name",
-  //   userEmail: "User Email",
-  //   userPhone: "User Phone",
-  //   deliveryDate: "Delivery Date",
-  //   price: "Price",
-  //   paymentStatus: "Successful",
-  //   editorName: "Ayman",
-  //   files: "https://drive.google.com/file/d/1/file1",
-  //   orderStatus: "Completed",
-  //   files2: "https://drive.google.com/file/d/file2",
-  //   icon: "icon",
+export const allColumns = [
+  "Order Date",
+  "Order ID",
+  "Order Name",
+  "Preview Edit Status",
+  "Remaining Days",
+  "Order Type",
+  "User's Name",
+  "User Email",
+  "User Phone",
+  "Delivery Date",
+  "Price",
+  "Payment Status",
+  "Editor",
+  "Files",
+  "Order Status",
+  "Files2",
+  "", // this field for icons
+];
 
-  //   previewstatus: "Approved",
-  //   remaining: "04/07 days",
-  // },
+const rowsData = [
   {
     date: "2023-07-19",
     id: "TX3857",
@@ -180,10 +163,209 @@ const rows = [
   },
 ];
 
+const allRowsData = [
+  {
+    date: "2023-07-19",
+    id: "TX1948",
+    orderName: "Mr XP",
+    orderType: "Express",
+    usersName: "John Doe",
+    userEmail: "john.doe@example.com",
+    userPhone: "123-456-7890",
+    deliveryDate: "2023-07-20",
+    price: "$100",
+    paymentStatus: "Successful",
+    editorName: "Ayman",
+    files: "https://drive.google.com/file/d/1/file1",
+    orderStatus: "Completed",
+    files2: "https://drive.google.com/file/d/1/file2",
+    previewstatus: "Approved",
+    remaining: "0/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-20",
+    id: "TX1949",
+    orderName: "Ms YZ",
+    orderType: "Standard",
+    usersName: "Jane Smith",
+    userEmail: "jane.smith@example.com",
+    userPhone: "234-567-8901",
+    deliveryDate: "2023-07-22",
+    price: "$150",
+    paymentStatus: "Pending",
+    editorName: "Bilal",
+    files: "https://drive.google.com/file/d/2/file1",
+    orderStatus: "In Progress",
+    files2: "https://drive.google.com/file/d/2/file2",
+    previewstatus: "Pending",
+    remaining: "2/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-21",
+    id: "TX1950",
+    orderName: "Dr AB",
+    orderType: "Express",
+    usersName: "Alice Brown",
+    userEmail: "alice.brown@example.com",
+    userPhone: "345-678-9012",
+    deliveryDate: "2023-07-21",
+    price: "$200",
+    paymentStatus: "Successful",
+    editorName: "Carlos",
+    files: "https://drive.google.com/file/d/3/file1",
+    orderStatus: "Completed",
+    files2: "https://drive.google.com/file/d/3/file2",
+    previewstatus: "Approved",
+    remaining: "0/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-22",
+    id: "TX1951",
+    orderName: "Mr CD",
+    orderType: "Standard",
+    usersName: "Chris Davis",
+    userEmail: "chris.davis@example.com",
+    userPhone: "456-789-0123",
+    deliveryDate: "2023-07-25",
+    price: "$250",
+    paymentStatus: "Failed",
+    editorName: "Dana",
+    files: "https://drive.google.com/file/d/4/file1",
+    orderStatus: "Failed",
+    files2: "https://drive.google.com/file/d/4/file2",
+    previewstatus: "Rejected",
+    remaining: "3/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-23",
+    id: "TX1952",
+    orderName: "Ms EF",
+    orderType: "Express",
+    usersName: "Eva Green",
+    userEmail: "eva.green@example.com",
+    userPhone: "567-890-1234",
+    deliveryDate: "2023-07-23",
+    price: "$300",
+    paymentStatus: "Successful",
+    editorName: "Eli",
+    files: "https://drive.google.com/file/d/5/file1",
+    orderStatus: "Completed",
+    files2: "https://drive.google.com/file/d/5/file2",
+    previewstatus: "Approved",
+    remaining: "0/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-24",
+    id: "TX1953",
+    orderName: "Dr GH",
+    orderType: "Standard",
+    usersName: "George Hill",
+    userEmail: "george.hill@example.com",
+    userPhone: "678-901-2345",
+    deliveryDate: "2023-07-27",
+    price: "$350",
+    paymentStatus: "Pending",
+    editorName: "Fiona",
+    files: "https://drive.google.com/file/d/6/file1",
+    orderStatus: "In Progress",
+    files2: "https://drive.google.com/file/d/6/file2",
+    previewstatus: "Pending",
+    remaining: "3/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-25",
+    id: "TX1954",
+    orderName: "Mr IJ",
+    orderType: "Express",
+    usersName: "Isaac Jones",
+    userEmail: "isaac.jones@example.com",
+    userPhone: "789-012-3456",
+    deliveryDate: "2023-07-25",
+    price: "$400",
+    paymentStatus: "Successful",
+    editorName: "Grace",
+    files: "https://drive.google.com/file/d/7/file1",
+    orderStatus: "Completed",
+    files2: "https://drive.google.com/file/d/7/file2",
+    previewstatus: "Approved",
+    remaining: "0/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-26",
+    id: "TX1955",
+    orderName: "Ms KL",
+    orderType: "Standard",
+    usersName: "Kara Lee",
+    userEmail: "kara.lee@example.com",
+    userPhone: "890-123-4567",
+    deliveryDate: "2023-07-29",
+    price: "$450",
+    paymentStatus: "Failed",
+    editorName: "Hannah",
+    files: "https://drive.google.com/file/d/8/file1",
+    orderStatus: "Failed",
+    files2: "https://drive.google.com/file/d/8/file2",
+    previewstatus: "Rejected",
+    remaining: "3/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-27",
+    id: "TX1956",
+    orderName: "Dr MN",
+    orderType: "Express",
+    usersName: "Mona Nelson",
+    userEmail: "mona.nelson@example.com",
+    userPhone: "901-234-5678",
+    deliveryDate: "2023-07-27",
+    price: "$500",
+    paymentStatus: "Successful",
+    editorName: "Ivy",
+    files: "https://drive.google.com/file/d/9/file1",
+    orderStatus: "Completed",
+    files2: "https://drive.google.com/file/d/9/file2",
+    previewstatus: "Approved",
+    remaining: "0/7 days",
+    icon: "",
+  },
+  {
+    date: "2023-07-28",
+    id: "TX1957",
+    orderName: "Mr OP",
+    orderType: "Standard",
+    usersName: "Oscar Perez",
+    userEmail: "oscar.perez@example.com",
+    userPhone: "012-345-6789",
+    deliveryDate: "2023-07-31",
+    price: "$550",
+    paymentStatus: "Pending",
+    editorName: "Jack",
+    files: "https://drive.google.com/file/d/10/file1",
+    orderStatus: "In Progress",
+    files2: "https://drive.google.com/file/d/10/file2",
+    previewstatus: "Pending",
+    remaining: "3/7 days",
+    icon: "",
+  },
+];
+
 // Extract unique editor names
-const uniqueEditors = [...new Set(rows.map((row) => row.editorName))];
+const uniqueEditors = [...new Set(rowsData.map((row) => row.editorName))];
 
 function OrderTable() {
+  // columns and rows
+  const [showAll, setShowAll] = useState(false);
+
+  const visibleColumns = showAll ? allColumns : columns;
+  const visibleRows = showAll ? allRowsData : rowsData;
+
   // Editors
   const [editorSelectedValues, setEditorSelectedValues] =
     useState("Assign Editor");
@@ -246,7 +428,7 @@ function OrderTable() {
           <Table className="simple w-full min-w-full">
             <TableHead>
               <TableRow>
-                {columns.map((column, index) => (
+                {visibleColumns.map((column, index) => (
                   <TableCell key={index}>
                     <Typography
                       color="text.secondary"
@@ -260,7 +442,7 @@ function OrderTable() {
             </TableHead>
 
             <TableBody>
-              {rows.map((row, index) => (
+              {visibleRows.map((row, index) => (
                 <TableRow key={index}>
                   {Object.entries(row).map(([key, value]) => {
                     switch (key) {
