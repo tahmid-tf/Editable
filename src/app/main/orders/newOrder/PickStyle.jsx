9e37ecf20d578a0d990ae33484b7dd6446f12953
rased <rased100@gmail.com>
2024-06-30 08:10:51 +0600

    pic-style: Additional info design

--- src/app/main/orders/newOrder/PickStyle.jsx
+++ src/app/main/orders/newOrder/PickStyle.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import StyleCard from "./StyleCards/StyleCard";
 import { Grid } from "@mui/material";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { Link } from "react-router-dom";
 
 // Validation Schema
 const validationSchema = Yup.object({
@@ -20,6 +21,8 @@ const PickStyle = () => {
           skinRetouching: false,
           previewEdits: false,
         },
+        totalImages: 5000,
+        driveLink: "",
       }}
       validationSchema={validationSchema}
       onSubmit={(values) => {
@@ -166,6 +169,7 @@ const PickStyle = () => {
                   Additional Edits
                 </p>
               </div>
+
               <label htmlFor="cullingCheckbox">
                 <div
                   style={{ cursor: "pointer" }}
@@ -226,6 +230,99 @@ const PickStyle = () => {
                   <p className="pl-10 text-[20px] font-bold">Preview Edits</p>
                 </div>
               </label>
+            </div>
+            <div className="my-60">
+              <div>
+                <p className="text-[32px] font-bold text-[#868686] py-36">
+                  Additional Info
+                </p>
+              </div>
+              <div className="">
+                <label
+                  htmlFor="totalImages"
+                  // className="text-md font-semibold text-black mt-16"
+                  className="font-semibold"
+                  style={{
+                    lineHeight: "20px",
+                    fontSize: "14px",
+                  }}
+                >
+                  How many images are you sending us?
+                </label>
+                <Field
+                  type="number"
+                  name="totalImages"
+                  placeholder=""
+                  className="mt-10 p-10 block w-[332px] h-[38px] border border-gray-300 rounded-md"
+                />
+                <ErrorMessage
+                  name="totalImages"
+                  component="div"
+                  className="text-red-500 text-xs mt-1"
+                />
+              </div>
+              <div className="mt-32">
+                <label
+                  htmlFor="driveLink"
+                  // className="text-md font-semibold text-black mt-16"
+                  className="font-semibold"
+                  style={{
+                    lineHeight: "20px",
+                    fontSize: "14px",
+                  }}
+                >
+                  Google Drive link with your images
+                </label>
+                <Field
+                  type="text"
+                  name="driveLink"
+                  placeholder="Paste your URL here"
+                  className="mt-10 p-10 block w-[332px] h-[38px] border border-gray-300 rounded-md"
+                />
+                <ErrorMessage
+                  name="driveLink"
+                  component="div"
+                  className="text-red-500 text-xs mt-1"
+                />
+              </div>
+            </div>
+            <div className="w-[400px] text-[12px] font-medium">
+              <p>Please ensure that:</p>
+              <div className="pl-5">
+                <div className="flex">
+                  <p>1.</p>
+                  <p className="pl-5">
+                    All the images have finished uploading in the drive before
+                    placing the order.{" "}
+                  </p>
+                </div>
+                <div className="flex">
+                  <p>2.</p>
+                  <p className="pl-5">
+                    Your images are either RAW photos, JPG photos, or Smart
+                    Preview Enabled Lightroom Catalogs.
+                  </p>
+                </div>
+              </div>
+
+              <p>
+                Find out how to create your own Smart Preview Enabled Lightroom
+                Catalogs{" "}
+                <Link to="#" className="" style={{ textDecoration: "none" }}>
+                  here
+                </Link>
+                .
+              </p>
+            </div>
+            <div className="pt-32 pb-24">
+              <button
+                type="submit"
+                // disabled={isSubmitting}
+                // onClick={onClose}
+                className="w-[332px] h-[38px] py-2  px-4 text-white rounded-md bg-[#146ef5ef] hover:bg-[#0066ff] font-[20px]"
+              >
+                Place Order
+              </button>
             </div>
             {errors.selectedStyle && touched.selectedStyle ? (
               <div style={{ color: "red" }}>{errors.selectedStyle}</div>
