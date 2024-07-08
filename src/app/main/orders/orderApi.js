b4c056d9dee1e2f3d9783f74e8248328ad076a4c
Abul Basar <76747711+AbulBashar38@users.noreply.github.com>
2024-07-08 15:23:28 +0600

    all filter is working

--- src/app/main/orders/orderApi.js
+++ src/app/main/orders/orderApi.js
@@ -3,8 +3,17 @@ import apiService from 'app/store/apiService';
 const orderApi = apiService.injectEndpoints({
 	endpoints: (builder) => ({
 		getOrdersData: builder.query({
-			query: ({ orderStatusValue, paymentStatusValue, editorValue, searchValue, rowPerPage, page }) =>
-				`admin/order_list?page=${page}${rowPerPage ? `&&paginate=${rowPerPage}` : ''}${searchValue ? `&&email=${searchValue}` : ''}${orderStatusValue ? `&&order_status=${orderStatusValue}` : ''}${paymentStatusValue ? `&&payment_status=${paymentStatusValue}` : ''}${editorValue ? `&&editor=${editorValue}` : ''}`
+			query: ({
+				orderStatusValue,
+				paymentStatusValue,
+				editorValue,
+				searchValue,
+				rowPerPage,
+				page,
+				startDate,
+				endDate
+			}) =>
+				`admin/order_list?page=${page}${rowPerPage ? `&&paginate=${rowPerPage}` : ''}${searchValue ? `&&email=${searchValue}` : ''}${orderStatusValue ? `&&order_status=${orderStatusValue}` : ''}${paymentStatusValue ? `&&payment_status=${paymentStatusValue}` : ''}${editorValue ? `&&editor=${editorValue}` : ''}${startDate ? `&&start_date=${startDate}` : ''}${endDate ? `&&end_date=${endDate}` : ''}`
 		})
 	})
 });
