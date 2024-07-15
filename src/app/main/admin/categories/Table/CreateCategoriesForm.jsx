1207c309d926f640186a0a9cd5fa1e423b822bc2
Abul Basar <76747711+AbulBashar38@users.noreply.github.com>
2024-07-15 14:30:33 +0600

    category CRUD is done

--- src/app/main/admin/categories/Table/CreateCategoriesForm.jsx
+++ src/app/main/admin/categories/Table/CreateCategoriesForm.jsx
@@ -1,247 +1,265 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import { IoClose } from "react-icons/io5";
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { IoClose } from 'react-icons/io5';
+import { Modal } from '@mui/material';
+import { useCreateCategoryMutation, useUpdateCategoryMutation } from '../CategoriesApi';
 
 const validationSchema = Yup.object().shape({
-  categoryName: Yup.string().required("Required"),
-  stylePrice: Yup.string().required("Required"),
-  styleThreshold: Yup.string().required("Required"),
-  cullingPrice: Yup.string().required("Required"),
-  cullingThreshold: Yup.string().required("Required"),
-  skinRetouchPrice: Yup.string().required("Required"),
-  skinRetouchThreshold: Yup.string().required("Required"),
-  previewEditPrice: Yup.string().required("Required"),
-  previewEditThreshold: Yup.string().required("Required"),
+	category_name: Yup.string().required('Required'),
+	style_price: Yup.string().required('Required'),
+	style_threshold: Yup.number().required('Required'),
+	culling_price: Yup.string().required('Required'),
+	culling_threshold: Yup.number().required('Required'),
+	skin_retouch_price: Yup.string().required('Required'),
+	skin_retouch_threshold: Yup.number().required('Required'),
+	preview_edit_price: Yup.string().required('Required'),
+	preview_edit_threshold: Yup.number().required('Required')
 });
 
-const CreateCategoriesForm = ({ onClose, successAlert }) => {
-  return (
-    <div className="p-24 bg-white shadow-md w-[390px] max-h-[80vh] overflow-y-auto">
-      <Formik
-        initialValues={{
-          categoryName: "",
-          stylePrice: "",
-          styleThreshold: "",
-          cullingPrice: "",
-          cullingThreshold: "",
-          skinRetouchPrice: "",
-          skinRetouchThreshold: "",
-          previewEditPrice: "",
-          previewEditThreshold: "",
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(values) => {
-          successAlert();
-          onClose();
-          console.log(values);
-        }}
-        className="rounded-xl"
-      >
-        {({ isSubmitting }) => (
-          <Form className="space-y-4 ">
-            <div className="flex items-center justify-between">
-              <p className="text-2xl font-bold text-[#868686]">
-                Create Category
-              </p>
-              <button onClick={onClose}>
-                <IoClose size={24} />
-              </button>
-            </div>
-            <div className="form-group">
-              <label
-                htmlFor="categoryName"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Category Name
-              </label>
-              <Field
-                type="text"
-                name="categoryName"
-                placeholder="Category Name"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="categoryName"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+const CreateCategoriesForm = ({ openModal, handleClose, successAlert, editedRowData }) => {
+	const [createCategory] = useCreateCategoryMutation();
+	const [updateCategory] = useUpdateCategoryMutation();
+	return (
+		<Modal
+			open={openModal}
+			onClose={handleClose}
+			aria-labelledby="modal-modal-title"
+			aria-describedby="modal-modal-description"
+			className="flex justify-center items-center"
+		>
+			<div className="bg-white flex justify-center items-center rounded-[5px]">
+				<div className="p-24 bg-white shadow-md w-[390px] max-h-[80vh] overflow-y-auto rounded-[5px]">
+					<Formik
+						initialValues={{
+							category_name: editedRowData?.category_name || '',
+							style_price: editedRowData?.style_price || '',
+							style_threshold: editedRowData?.style_threshold || '',
+							culling_price: editedRowData?.culling_price || '',
+							culling_threshold: editedRowData?.culling_threshold || '',
+							skin_retouch_price: editedRowData?.skin_retouch_price || '',
+							skin_retouch_threshold: editedRowData?.skin_retouch_threshold || '',
+							preview_edit_price: editedRowData?.preview_edit_price || '',
+							preview_edit_threshold: editedRowData?.preview_edit_threshold || ''
+						}}
+						validationSchema={validationSchema}
+						onSubmit={async (values) => {
+							console.log(values);
+							const response =
+								editedRowData !== null
+									? await updateCategory({ body: values, id: editedRowData?.id })
+									: await createCategory(values);
+							if (response.data) {
+								successAlert();
+								handleClose();
+							}
+						}}
+						className="rounded-xl"
+					>
+						{({ isSubmitting }) => (
+							<Form className="space-y-4 ">
+								<div className="flex items-center justify-between">
+									<p className="text-2xl font-bold text-[#868686]">Create Category</p>
+									<button onClick={handleClose}>
+										<IoClose size={24} />
+									</button>
+								</div>
+								<div className="form-group">
+									<label
+										htmlFor="category_name"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Category Name
+									</label>
+									<Field
+										type="text"
+										name="category_name"
+										placeholder="Category Name"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="category_name"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="stylePrice"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Style Price ($)
-              </label>
-              <Field
-                type="text"
-                name="stylePrice"
-                placeholder="Price in USD"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="stylePrice"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+								<div className="form-group">
+									<label
+										htmlFor="style_price"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Style Price ($)
+									</label>
+									<Field
+										type="text"
+										name="style_price"
+										placeholder="Price in USD"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="style_price"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="styleThreshold"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Style Threshold
-              </label>
-              <Field
-                type="text"
-                name="styleThreshold"
-                placeholder="Enter the minimum required for this service"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="styleThreshold"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+								<div className="form-group">
+									<label
+										htmlFor="style_threshold"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Style Threshold
+									</label>
+									<Field
+										type="number"
+										name="style_threshold"
+										placeholder="Enter the minimum required for this service"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="style_threshold"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="cullingPrice"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Culling Price ($)
-              </label>
-              <Field
-                type="text"
-                name="cullingPrice"
-                placeholder="Price in USD"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="cullingPrice"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+								<div className="form-group">
+									<label
+										htmlFor="culling_price"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Culling Price ($)
+									</label>
+									<Field
+										type="text"
+										name="culling_price"
+										placeholder="Price in USD"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="culling_price"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="cullingThreshold"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Culling Threshold
-              </label>
-              <Field
-                type="text"
-                name="cullingThreshold"
-                placeholder="Enter the minimum required for this service"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="cullingThreshold"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+								<div className="form-group">
+									<label
+										htmlFor="culling_threshold"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Culling Threshold
+									</label>
+									<Field
+										type="number"
+										name="culling_threshold"
+										placeholder="Enter the minimum required for this service"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="culling_threshold"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="skinRetouchPrice"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Skin Retouch Price ($)
-              </label>
-              <Field
-                type="text"
-                name="skinRetouchPrice"
-                placeholder="Price in USD"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="skinRetouchPrice"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+								<div className="form-group">
+									<label
+										htmlFor="skin_retouch_price"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Skin Retouch Price ($)
+									</label>
+									<Field
+										type="number"
+										name="skin_retouch_price"
+										placeholder="Price in USD"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="skin_retouch_price"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="skinRetouchThreshold"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Skin Retouch Threshold
-              </label>
-              <Field
-                type="text"
-                name="skinRetouchThreshold"
-                placeholder="Enter the minimum required for this service"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="skinRetouchThreshold"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+								<div className="form-group">
+									<label
+										htmlFor="skin_retouch_threshold"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Skin Retouch Threshold
+									</label>
+									<Field
+										type="number"
+										name="skin_retouch_threshold"
+										placeholder="Enter the minimum required for this service"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="skin_retouch_threshold"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="previewEditPrice"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Preview Edit Price ($)
-              </label>
-              <Field
-                type="text"
-                name="previewEditPrice"
-                placeholder="Price in USD"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="previewEditPrice"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
+								<div className="form-group">
+									<label
+										htmlFor="preview_edit_price"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Preview Edit Price ($)
+									</label>
+									<Field
+										type="text"
+										name="preview_edit_price"
+										placeholder="Price in USD"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="preview_edit_price"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
 
-            <div className="form-group">
-              <label
-                htmlFor="previewEditThreshold"
-                className="block text-md font-medium text-gray-700 mt-16"
-              >
-                Preview Edit Threshold
-              </label>
-              <Field
-                type="text"
-                name="previewEditThreshold"
-                placeholder="Enter the minimum required for this service"
-                className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
-              />
-              <ErrorMessage
-                name="previewEditThreshold"
-                component="div"
-                className="text-red-500 text-xs mt-1"
-              />
-            </div>
-            <div className="pt-32 pb-24">
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                // onClick={onClose}
-                className="w-full h-[38px] py-2  px-4 text-white rounded-md bg-[#146ef5ef] hover:bg-[#0066ff]"
-              >
-                Create Category
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+								<div className="form-group">
+									<label
+										htmlFor="preview_edit_threshold"
+										className="block text-md font-medium text-gray-700 mt-16"
+									>
+										Preview Edit Threshold
+									</label>
+									<Field
+										type="number"
+										name="preview_edit_threshold"
+										placeholder="Enter the minimum required for this service"
+										className="mt-10 p-10 block w-full h-[38px] border border-gray-300 rounded-md"
+									/>
+									<ErrorMessage
+										name="preview_edit_threshold"
+										component="div"
+										className="text-red-500 text-xs mt-1"
+									/>
+								</div>
+								<div className="pt-32 pb-24">
+									<button
+										type="submit"
+										disabled={isSubmitting}
+										// onClick={onClose}
+										className="w-full h-[38px] py-2  px-4 text-white rounded-md bg-[#146ef5ef] hover:bg-[#0066ff]"
+									>
+										{editedRowData !== null ? 'Update Category' : 'Create Category'}
+									</button>
+								</div>
+							</Form>
+						)}
+					</Formik>
+				</div>
+			</div>
+		</Modal>
+	);
 };
 
 export default CreateCategoriesForm;
